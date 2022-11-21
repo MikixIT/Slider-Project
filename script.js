371c7ecad4a999271d0d9569b2079854da0dfda8
MikixIT <michael.46@live.it>
2022-11-21 20:31:54 +0100

    Starting with bottom buttons

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const log = (x) => {console.log(x)};
 const leftArrow = document.querySelector('.left');
 const rightArrow = document.querySelector('.right');
 const slider = document.querySelector('.slider');
-let images = document.querySelectorAll('.images')
+let images = document.querySelectorAll('.images');
+const bottom = document.querySelector('.bottom');
 
 let lengthImg = images.length
 let sliderCounter = 1;
 
+//Slider Function
 const nextSlide = () => {
     slider.style.transform = `translateX(-${sliderCounter * 900}px)`;
     sliderCounter++;
@@ -28,6 +30,7 @@ const lastSlide = () => {
     sliderCounter = lengthImg;
 };
 
+// Event Listeners
 rightArrow.addEventListener("click", () => {
     sliderCounter < lengthImg ? nextSlide() : startSlide(); 
 });
@@ -36,4 +39,6 @@ leftArrow.addEventListener("click", () => {
     sliderCounter > 1 ? prevSlide() : lastSlide()
 })
 
+
+//Bottom buttons
 
