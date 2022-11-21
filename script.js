94251f3ec240bd77ae1bb64c87cec8e0395d6963
MikixIT <michael.46@live.it>
2022-11-21 19:28:58 +0100

    First project commit!

---
+++ script.js
@@ -0,0 +1,39 @@
+const log = (x) => {console.log(x)};
+
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
+const slider = document.querySelector('.slider');
+let images = document.querySelectorAll('.images')
+
+let lengthImg = images.length
+let sliderCounter = 1;
+
+const nextSlide = () => {
+    slider.style.transform = `translateX(-${sliderCounter * 900}px)`;
+    sliderCounter++;
+};
+
+const prevSlide = () => {
+    slider.style.transform = `translateX(-${(sliderCounter - 2) * 900}px)`;
+    sliderCounter--;
+};
+
+const startSlide = () => {
+    slider.style.transform = `translateX(0px)`;
+    sliderCounter = 1;
+};
+
+const lastSlide = () => {
+    slider.style.transform = `translateX(-${(lengthImg - 1) * 900}px)`;
+    sliderCounter = lengthImg;
+};
+
+rightArrow.addEventListener("click", () => {
+    sliderCounter < lengthImg ? nextSlide() : startSlide(); 
+});
+
+leftArrow.addEventListener("click", () => {
+    sliderCounter > 1 ? prevSlide() : lastSlide()
+})
+
+
